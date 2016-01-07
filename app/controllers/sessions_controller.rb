6eb0b24cf17b8a75a1d4da55a8fa2a1f460de8a7
tvcou <tvcou.zyx77@gmail.com>
2016-01-07 20:29:29 +0000

    "make sessions_controller.rb vi app/controllers/sessions_controller.rb"

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(name: params[:session][:name])
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
 end
