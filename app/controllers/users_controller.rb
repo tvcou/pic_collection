34f0a3eef25ed9eff9c12f2fd48cbed14de0225d
tvcou <tvcou.zyx77@gmail.com>
2016-01-07 02:34:48 +0000

    "vi app/controllers/users_controller.rb"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,48 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+ 
+  def update
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private def user_params
+    params[:user].permit(:name, :email) 
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
