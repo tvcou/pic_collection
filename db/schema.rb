88833da45dc63ba685e226acdb5f88afb03e9cdf
tvcou <tvcou.zyx77@gmail.com>
2016-01-07 02:36:22 +0000

    "rake db:migrate"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107020112) do
+ActiveRecord::Schema.define(version: 20160107023542) do
 
   create_table "blocks", force: :cascade do |t|
     t.string   "name"
@@ -25,8 +25,9 @@ ActiveRecord::Schema.define(version: 20160107020112) do
     t.string   "name"
     t.string   "email"
     t.integer  "block_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_index "users", ["block_id"], name: "index_users_on_block_id"
