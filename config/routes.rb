c89dafcbfb873ffbec0f56532ad1b43f0d32917b
tvcou <tvcou.zyx77@gmail.com>
2016-01-07 20:26:02 +0000

    "add login routing vi config/routes.rb"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   resources :blocks
   resources :users
+  get 'signup' => 'users#new'
   root 'blocks#index'
 
   get '/collection' => 'blocks#collection'
+
+  get     'login' => 'sessions#new'
+  post    'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
 end
