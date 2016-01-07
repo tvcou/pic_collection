bec44f60c74d5bef3d1f65f5ef8f0f03e8884c29
tvcou <tvcou.zyx77@gmail.com>
2016-01-07 03:11:13 +0000

    "add password save vi app/controllers/users_controller.rb app/views/users/_form.html.erb"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
   end
 
   private def user_params
-    params[:user].permit(:name, :email) 
+    params[:user].permit(:name, :email, :password, :password_confirmation) 
   end
 
   def set_user
